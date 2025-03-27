e70021df3a2a692ecac8c8cc2b2773a324a5ec3b
Okuhle charlieman <okuhlecharlieman72@gmail.com>
2025-03-27 21:10:06 +0200

    added a qoute class

---
+++ demo/src/main/java/com/example/demo/Quote.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+public class Quote {
+    private String text;
+    private String author;
+
+    public Quote(String text, String author) {
+        this.text = text;
+        this.author = author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
